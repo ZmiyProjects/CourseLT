6150fbccc4d00afc3ba8c98b15c765bb59f65be6
ZmiyProjects <stud4597@yandex.ru>
2020-05-09 03:26:56 +0300

    doc

--- CourseLT.sql
+++ CourseLT.sql
@@ -90,7 +90,8 @@ CREATE TABLE Study.AcademicGroup(
     GroupId INT PRIMARY KEY DEFAULT NEXT VALUE FOR Study.SeqAcademicGroup,
     GroupName VARCHAR(50) NOT NULL UNIQUE,
     Limit SMALLINT NOT NULL,
-    SpecializationId INT NOT NULL REFERENCES Study.Specialization(SpecializationId)
+    SpecializationId INT NOT NULL REFERENCES Study.Specialization(SpecializationId),
+    CONSTRAINT CK_Limit CHECK (Limit BETWEEN 0 AND 25)
 );
 
 CREATE TABLE Study.StudentGroup(
@@ -116,13 +117,18 @@ CREATE TABLE Archive.ModuleArchive(
 );
 
 GO
+-- Возвращает количество студентов в определенгой группе, если она существует, иначе NULL
+-- @GroupId - идентификато группы, для которой вычисляется количество студентов
 CREATE OR ALTER FUNCTION Study.count_members(@GroupId INT) RETURNS INT AS
     BEGIN
+        IF @GroupId NOT IN (SELECT GroupId FROM Study.AcademicGroup)
+            RETURN NULL;
         DECLARE @Result INT = (SELECT COUNT(GroupId) FROM Study.StudentGroup WHERE GroupId = @GroupId);
         RETURN @Result;
     END
 GO
 
+-- Триггер, вызывающий исключение при попытке вставки в группы чрезмерного числа студентов (больше чем Study.AcademicGroup.Limit)
 CREATE TRIGGER Study.check_group_limit ON Study.StudentGroup AFTER INSERT AS
     BEGIN
         IF EXISTS(
@@ -133,21 +139,37 @@ CREATE TRIGGER Study.check_group_limit ON Study.StudentGroup AFTER INSERT AS
     END
 
 GO
+-- Нельзя установить минимальное число студентов меньше, чем уже присутствует в группе
+CREATE TRIGGER Study.check_group_limit_update ON Study.AcademicGroup AFTER UPDATE AS
+    BEGIN
+        IF UPDATE(Limit)
+        BEGIN
+            IF EXISTS(
+                SELECT * FROM inserted AS I
+                    WHERE I.Limit < Study.count_members(I.GroupId)
+                )
+            THROW 51010, N'Нельзя установить минимальное число студентов меньше, чем уже присутствует в группе!', 10;
+        END
+    END
 
-CREATE VIEW Study.GroupInfo AS
+GO
+-- Представления с полными сведениями о группе, включая сведения о специальности к которой она отностися
+-- и количевство учащихся (members)
+CREATE OR ALTER VIEW Study.GroupInfo AS
     SELECT
            AG.GroupId,
            AG.GroupName,
-           CAST(ISNULL(Se.SemesterNumber, N'Обучение не начато') AS VARCHAR(25)) AS Semester,
            S.SpecializationName,
            S.Price,
            AG.Limit AS max_members,
            Study.count_members(AG.GroupId) AS members
     FROM Study.AcademicGroup AS AG
         JOIN Study.Specialization AS S ON AG.SpecializationId = S.SpecializationId
-        JOIN Study.Semester AS Se ON S.SpecializationId = Se.SpecializationId
 GO
 
+-- Вставка новой специализации
+-- @Name - название специальности, в базе не должно быть одноименной специальности
+-- @Duration - продолжительность обучения в семестрах (от 2 до 4)
 CREATE OR ALTER PROCEDURE Study.new_specialization
     @Name VARCHAR(255),
     @Duration SMALLINT
@@ -165,6 +187,9 @@ AS
     END
 
 GO
+-- Вычисление суммарной стоимости курсов, входящих в специализацию,
+-- если специальность с указанным идентификатором отсутствует - вернет NULL
+-- @SpecializationId идентификатор специальности, для которой осуществляется вычисление
 CREATE OR ALTER FUNCTION Study.sum_course_price(@SpecializationId INT) RETURNS NUMERIC(9, 2) AS
     BEGIN
         DECLARE @SumPrice NUMERIC(9, 2) = (
@@ -175,12 +200,16 @@ CREATE OR ALTER FUNCTION Study.sum_course_price(@SpecializationId INT) RETURNS N
         RETURN @SumPrice;
     END
 GO
-CREATE VIEW Study.AllCourses WITH SCHEMABINDING AS
-    SELECT C.CourseId, C.CourseName, C.Price FROM Study.Course AS C
+
+-- Представление демонстрирующее все зарегиситрированные в системе курсы, включая архивные
+CREATE OR ALTER VIEW Study.AllCourses WITH SCHEMABINDING AS
+    SELECT C.CourseId, C.CourseName, C.Price, N'Действующий' AS status FROM Study.Course AS C
     UNION
-    SELECT CA.CourseId, CA.CourseName, CA.Price FROM Archive.CourseArchive AS CA;
+    SELECT CA.CourseId, CA.CourseName, CA.Price, N'Архивный' AS status FROM Archive.CourseArchive AS CA;
 GO
 
+-- Проверяет наличие курса с указанным названием в архиве, если присутствует вернет 1, иначе 0
+-- @Name - название курса, для которого осуществляется проверка
 CREATE OR ALTER FUNCTION Study.check_course_in_archive(@Name VARCHAR(255)) RETURNS BIT AS
     BEGIN
         IF @Name IN (SELECT CourseName FROM Archive.CourseArchive)
@@ -190,7 +219,7 @@ CREATE OR ALTER FUNCTION Study.check_course_in_archive(@Name VARCHAR(255)) RETUR
 
 GO
 
--- Проверка наличия одноименного курса в архиве, выполняемая при добавлении и переименовании курса
+-- Триггер, изменяющий стоимость специализации, при изменении цены входящего в неё курса
 CREATE OR ALTER TRIGGER Study.check_course ON Study.Course AFTER UPDATE AS
     BEGIN
         IF UPDATE(Price)
@@ -204,6 +233,9 @@ CREATE OR ALTER TRIGGER Study.check_course ON Study.Course AFTER UPDATE AS
     END
 GO
 
+-- Создание нового курса, с учетом ранее обозначенных ограничений
+-- @Name - название курса, в базе (учитывая архив) не должно быть одноименной специальности
+-- @Price - стоимость курса, 0, если курс бесплатный
 CREATE OR ALTER PROCEDURE Study.new_course
     @Name VARCHAR(255),
     @Price NUMERIC(9, 2)
@@ -215,6 +247,10 @@ AS
     END
 
 GO
+
+-- Переименование курса
+-- @CourseId - идентификатор курса, если в базе откутствует курс с заданным идентификаторум - будет вызвано исключение
+-- @NewName - новое наименование курса, с учетом ранее обозначенных правил именование (не должно повторяться в БД, включая архив)
 CREATE OR ALTER PROCEDURE Study.rename_course
     @CourseId INT,
     @NewName VARCHAR(255)
@@ -228,6 +264,7 @@ AS
     END
 
 GO
+
 -- Удаление и архивирование курса со всеми его модулями
 CREATE TRIGGER Study.archiving_course ON Study.Course INSTEAD OF DELETE AS
     BEGIN
@@ -245,7 +282,10 @@ CREATE TRIGGER Study.archiving_course ON Study.Course INSTEAD OF DELETE AS
             WHERE CourseId IN (SELECT CourseId FROM deleted);
     END
 GO
+
 -- Восстанавливает из архива курс и все его модули
+-- @CourseId - идентификатор архивного курса, который необходимо восстановить,
+-- если такого курса нет в архиве - будет вызвано исключение
 CREATE OR ALTER PROCEDURE Study.restore_course
     @CourseId INT
 AS
@@ -274,6 +314,7 @@ AS
             SELECT ModuleId, ModuleName, ModuleDescription, CourseId FROM @Module
     END
 GO
+-- изменение стоимости специализации в зависимости от назначения и/или удаления курсов на входящие в специализацию семестры
 CREATE TRIGGER Study.calculate_price ON Study.Curriculum AFTER INSERT, UPDATE, DELETE AS
     BEGIN
         UPDATE Study.Specialization SET
@@ -284,70 +325,72 @@ CREATE TRIGGER Study.calculate_price ON Study.Curriculum AFTER INSERT, UPDATE, D
 
 GO
 
-INSERT INTO Study.Student(FirstName, LastName, MiddleName, BirthDate, Phone, Email, Gender) VALUES
-    ('Иванов', 'Иван', 'Иванович', '20000102', '83452123233', 'ds2122@gmail.com', 'Мужчина');
-INSERT INTO Study.Student(FirstName, LastName, MiddleName, BirthDate, Phone, Email, Gender) VALUES
-    ('Петров', 'сергей', 'Иванович', '19980308', '83853123003', 'ufg2122@gmail.com', 'Мужчина');
-INSERT INTO Study.Student(FirstName, LastName, MiddleName, BirthDate, Phone, Email, Gender) VALUES
-    ('Сергеева', 'Вероника', 'Алексеевна', '20011009', '85530002200', 'vs3232@gmail.com', 'Женщина');
-INSERT INTO Study.Student(FirstName, LastName, MiddleName, BirthDate, Phone, Email, Gender) VALUES
-    ('Петров', 'Кирилл', 'Генадьевич', '19940729', '83452123477', 'kg321@gmail.com', 'Мужчина');
+-- Роли и пользователи
+CREATE ROLE CourseLTManager;
 
-EXEC Study.new_specialization 'Программирование на Python', 2;
+GRANT SELECT ON SCHEMA::Archive TO CourseLTManager;
 
-INSERT INTO Study.AcademicGroup(GroupName, Limit, SpecializationId) VALUES ('P-1', 3, 1);
-INSERT INTO Study.AcademicGroup(GroupName, Limit, SpecializationId) VALUES ('P-2', 2, 1);
+GRANT EXECUTE, SELECT, INSERT, UPDATE, DELETE ON SCHEMA::Study TO CourseLTManager;
 
-INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (1, 1);
-INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (2, 1);
-INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (3, 1);
+DENY INSERT ON Study.Course TO CourseLTManager;
+DENY INSERT ON Study.Specialization TO CourseLTManager;
+DENY UPDATE ON Study.Specialization(Duration, Price) TO CourseLTManager;
+DENY DELETE, UPDATE ON Study.Semester TO CourseLTManager;
+DENY UPDATE ON Study.Course(CourseName) TO CourseLTManager;
 
-INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (2, 2);
-INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (3, 2);
+IF EXISTS(SELECT * FROM sys.server_principals WHERE name = 'CourseLTManagerOne')
+    DROP LOGIN CourseLTManagerOne;
 
-EXEC Study.new_course 'Основы Python', 3000;
-EXEC Study.new_course 'Системы контроля версий и git', 2000;
-EXEC Study.new_course 'Базы данных', 4000;
+CREATE LOGIN CourseLTManagerOne WITH
+    PASSWORD = 'test',
+    DEFAULT_DATABASE = CourseLT;
 
-INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES ('Введение', 'Информация о курсе', 1);
-INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES ('Переменные', 'Ввод/вывод', 1);
-INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES ('Условные операторы', 'if..elif..else', 1);
-INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES ('Циклы', 'for, while', 1);
+CREATE USER CourseLTManagerOne FOR LOGIN CourseLTManagerOne;
 
-INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES ('Введение', 'Назначение системы контроля версий', 2);
-INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES ('Базовые команды', 'add, commit, log', 2);
-INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES ('Хранение в облаке', 'Знакомство с GitHub', 2);
+ALTER ROLE CourseLTManager
+    ADD MEMBER CourseLTManagerOne;
 
-INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES ('Основы проектирования БД', 'ER-модель, нормализация', 3);
-INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES ('Основы SQL', 'CRUD операции', 3);
-
-INSERT INTO Study.Curriculum(CourseId, SemesterId) VALUES (1, 1);
-INSERT INTO Study.Curriculum(CourseId, SemesterId) VALUES (2, 1);
-INSERT INTO Study.Curriculum(CourseId, SemesterId) VALUES (3, 2);
-
-SELECT * FROM Study.Specialization;
-SELECT * FROM Study.Semester;
-
-SELECT * FROM Study.Curriculum;
-
-SELECT * FROM Study.Course;
-
-UPDATE Study.Course SET
-    Price = 1000
-WHERE CourseId = 3;
+-- Заполнение для тестирования
+EXECUTE AS LOGIN = 'CourseLTManagerOne'
+    INSERT INTO Study.Student(FirstName, LastName, MiddleName, BirthDate, Phone, Email, Gender) VALUES
+        (N'Иванов', N'Иван', N'Иванович', '20000102', '83452123233', 'ds2122@gmail.com', N'Мужчина');
+    INSERT INTO Study.Student(FirstName, LastName, MiddleName, BirthDate, Phone, Email, Gender) VALUES
+        (N'Петров', N'Сергей', N'Иванович', '19980308', '83853123003', 'ufg2122@gmail.com', N'Мужчина');
+    INSERT INTO Study.Student(FirstName, LastName, MiddleName, BirthDate, Phone, Email, Gender) VALUES
+        (N'Сергеева', N'Вероника', N'Алексеевна', '20011009', '85530002200', 'vs3232@gmail.com', N'Женщина');
+    INSERT INTO Study.Student(FirstName, LastName, MiddleName, BirthDate, Phone, Email, Gender) VALUES
+        (N'Петров', N'Кирилл', N'Генадьевич', '19940729', '83452123477', 'kg321@gmail.com', N'Мужчина');
 
 
+    EXEC Study.new_specialization N'Программирование на Python', 2;
 
-SELECT * FROM Study.Course;
+    INSERT INTO Study.AcademicGroup(GroupName, Limit, SpecializationId) VALUES ('P-1', 8, 1);
+    INSERT INTO Study.AcademicGroup(GroupName, Limit, SpecializationId) VALUES ('P-2', 5, 1);
 
-SELECT * FROM Archive.CourseArchive AS C
-    JOIN Archive.ModuleArchive AS M ON C.CourseId = M.CourseId;
+    INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (1, 1);
+    INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (2, 1);
+    INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (3, 1);
 
-SELECT C.CourseId, C.CourseName, M.ModuleName FROM Study.Course AS C
-    JOIN Study.Module AS M ON C.CourseId = M.CourseId;
+    INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (2, 2);
+    INSERT INTO Study.StudentGroup(StudentId, GroupId) VALUES (3, 2);
 
-DELETE FROM Study.Course
-    WHERE CourseId = 1;
+    EXEC Study.new_course N'Основы Python', 3000;
+    EXEC Study.new_course N'Системы контроля версий и git', 2000;
+    EXEC Study.new_course N'Базы данных', 4000;
 
-EXEC Study.restore_course 1;
+    INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES (N'Введение', N'Информация о курсе', 1);
+    INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES (N'Переменные', N'Ввод/вывод', 1);
+    INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES (N'Условные операторы', 'if..elif..else', 1);
+    INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES (N'Циклы', 'for, while', 1);
 
+    INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES (N'Введение', N'Назначение системы контроля версий', 2);
+    INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES (N'Базовые команды', 'add, commit, log', 2);
+    INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES (N'Хранение в облаке', N'Знакомство с GitHub', 2);
+
+    INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES (N'Основы проектирования БД', N'ER-модель, нормализация', 3);
+    INSERT INTO Study.Module(ModuleName, ModuleDescription, CourseId) VALUES (N'Основы SQL', N'CRUD операции', 3);
+
+    INSERT INTO Study.Curriculum(CourseId, SemesterId) VALUES (1, 1);
+    INSERT INTO Study.Curriculum(CourseId, SemesterId) VALUES (2, 1);
+    INSERT INTO Study.Curriculum(CourseId, SemesterId) VALUES (3, 2);
+REVERT;
